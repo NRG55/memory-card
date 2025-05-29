21caeaf6e91eaba1ce3ce2fe7b05819c44b77105
NRG55 <maloj_1980@yahoo.fr>
2025-05-29 20:06:50 +0100

    Update(components): Card.jsx and Gameboard.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,24 @@
-export default function Card({ cardTitle, imageUrl}) { 
+import cardBack6 from "../images/card-back6.jpg";
+
+export default function Card({ cardTitle, imageUrl, cardId, isFlipped, handleClick}) { 
     return (
         <>
-            <div className="card">
-                <div>
-                    <img src={imageUrl} alt={cardTitle}/>
+            <div id={cardId} 
+                 className={`card ${isFlipped ? "flipped disabled" : ""}`} 
+                 onClick={handleClick}
+            > 
+                <div className="card-face">
+                    <img
+                        src={imageUrl}
+                        alt={cardTitle}              
+                    />
                 </div>
-                <h3>{cardTitle}</h3>
+                <div className="card-back">
+                    <img
+                        src={cardBack6}
+                        alt="Card back"
+                    />
+                </div>                                    
             </div>
         </>
     );
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,13 +1,21 @@
 import Card from "./Card";
 
-export default function Gameboard({ cards }) {
+export default function Gameboard({ cards, score, bestScore, isFlipped, handleClick,}) {
+ 
     return (
-        <section className="gameboard-section">            
-            <h2>Score</h2>
-            <h2>Best Score</h2>
-            <div className="cards-container">
+        <section className="gameboard-section">
+            <div className="score">            
+                <div className="score-text">Score: <span className="score-number">{score}</span></div>
+                <div className="score-text">Best Score: <span className="score-number">{bestScore}</span></div>
+            </div>
+            <div className={`cards-container`}>
                 {cards.map((card) =>
-                    <Card key={card.id} cardTitle={card.title} imageUrl={card.imageUrl}/>
+                    <Card key={card.id} 
+                          cardId={card.id} 
+                          cardTitle={card.title} 
+                          imageUrl={card.imageUrl}
+                          isFlipped={isFlipped} 
+                          handleClick={handleClick}/>
                 )}
             </div>
         </section>
